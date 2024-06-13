afbec76983e93bfad62c89d82b57d7c8be490d93
Luca Bulles <luca.bulles@alten.nl>
2024-06-13 11:35:55 +0200

    test for pipeline trigger of day_2

--- Day_2/main.cpp
+++ Day_2/main.cpp
@@ -5,7 +5,6 @@
 //first is opponent, third is own response
 // opponent choices: A = rock, B = paper, C = scissors
 // own choice: X = rock, Y = paper, Z = scissors
-
 //points: 6 = win, 3 = tie, 0 = lose
 //points rock = 1, paper = 2, scissors = 3
 
